c1be89f883f6829c2cc3461428863beabad845d6
lee unseo <lee.unseo17@gmail.com>
2022-04-19 23:01:43 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use clap::{arg, command, ArgMatches, Command};
+
+fn main() {
+    let matches = command!()
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("login")
+                .about("Login with your AtCoder account.")
+                .arg(arg!(--user <VALUE>))
+                .arg(arg!(--pass <VALUE>)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("login", sub_matches)) => login(sub_matches),
+        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
+    }
+}
+
+fn login(matches: &ArgMatches) {
+    println!(
+        "user:{:?} pass:{:?}",
+        matches.value_of("user").unwrap(),
+        matches.value_of("pass").unwrap(),
+    )
+}
