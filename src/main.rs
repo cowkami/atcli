b5a076b8313aabae9a478d307526cae1c4222594
lee unseo <lee.unseo17@gmail.com>
2022-04-24 13:29:58 +0900

    deprecated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{arg, command, ArgMatches, Command};
+use serde::Deserialize;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let matches = command!()
         .propagate_version(true)
         .subcommand_required(true)
@@ -8,7 +10,7 @@ fn main() {
         .subcommand(
             Command::new("authorize")
                 .about("Get your verification code.")
-                .arg(arg!(<USER>)),
+                .arg(arg!([USER])),
         )
         .subcommand(
             Command::new("login")
@@ -20,7 +22,8 @@ fn main() {
 
     match matches.subcommand() {
         Some(("authorize", sub_matches)) => {
-            authorize(sub_matches);
+            let verification_code = authorize(sub_matches).await;
+            println!("Your verification code: {:?}", verification_code);
             println!("Register this verification code to your account's Affiliation.");
         }
         Some(("login", sub_matches)) => {
@@ -31,15 +34,24 @@ fn main() {
     }
 }
 
-async fn authorize(matches: &ArgMatches) -> reqwest::Result<()> {
+#[derive(Deserialize)]
+struct IAuthorizeResponse {
+    verification_code: String,
+}
+
+async fn authorize(matches: &ArgMatches) -> reqwest::Result<String> {
+    let user_id = matches.value_of("USER").unwrap();
+    let params = [("user_id", user_id)];
     let client = reqwest::Client::new();
     let res = client
-        .post("http://httpbin.org/post")
-        .body("the exact body that is sent")
+        .post("https://atcoder-auth.kenkoooo.com/api/authorize")
+        .form(&params)
         .send()
+        .await?
+        .json::<IAuthorizeResponse>()
         .await?;
-
-    Ok(())
+    println!("hello");
+    Ok(res.verification_code)
 }
 
 async fn login(matches: &ArgMatches) -> reqwest::Result<()> {
